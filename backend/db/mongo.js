4809a83e81a78326a9537eafa04499407b528922
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-09-28 19:56:40 +0200

    Integrate MongoDB connection and keep in-memory users with admin login

---
+++ backend/db/mongo.js
@@ -0,0 +1,6 @@
+const password = 'sm2oF8HNnDlJ82uz'
+const user = 'eh_sly'
+const DB_URL = `mongodb+srv://${user}:${password}@monvieuxgrimoire.nukhpjv.mongodb.net/?retryWrites=true&w=majority&appName=monVieuxGrimoire`
+console.log("DB_url :", DB_URL)
+
+module.exports = {};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("./db/mongo.js");
 const port = 4000;
 
 app.use(cors());
